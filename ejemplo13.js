7351d3b5c263a6323018b383dc550a005a16d3c0
Juan <alonso.jad@gmail.com>
2024-05-07 13:51:15 +0200

    Update ejemplo13.js

--- ejemplo13.js
+++ ejemplo13.js
@@ -1,15 +1,26 @@
 
+/* Esta función recibe tres colores (con valores de 0 a 255) y devuelve una cadena
+con rgb y los colores, que es la propiedad CSS. */
+
 function rgb(rojo, verde, azul) {
-    return `rgb(${rojo}, ${verde}, ${azul}`;
+    /* Usando plantillas de cadenas, llamadas interpolación en otros lenguajes,
+    podemos crear una cadena sin tener que andar abriendo y cerrando comillas ni 
+    uniéndolas con + */
+    return `rgb(${rojo}, ${verde}, ${azul})`;
+    // Sin interpolación sería:
+    //return "rgb("+ rojo + "," + verde + "," + azul + ")";
 }
 function cambiar() {
+    /* Para cambiar el color debemos tener en cuenta los tres ranges. Por ello usamos
+    la función rgb que hemos creado arriba */
     document.getElementById("texto").style.color = 
-    rgb(document.getElementById("rojo").value,
-    document.getElementById("verde").value, 
-    document.getElementById("azul").value);
+        rgb(document.getElementById("rojo").value,
+        document.getElementById("verde").value, 
+        document.getElementById("azul").value);
 }
+/* En este ejercicio vamos a usar una misma función para todos los range.
+El evento input se produce cada vez que el usuario va moviendo el "deslizador" del range
+*/
 document.getElementById("rojo").addEventListener("input", cambiar);
 document.getElementById("verde").addEventListener("input", cambiar);
 document.getElementById("azul").addEventListener("input", cambiar);
-
-
