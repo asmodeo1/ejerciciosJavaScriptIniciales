0b30b84da431d164a95348fad4637d1b355e40a3
Juan <alonso.jad@gmail.com>
2024-05-07 13:36:14 +0200

    Add files via upload

--- ejemplo12.js
+++ ejemplo12.js
@@ -10,6 +10,8 @@ function ponerAzul() {
     document.getElementById("texto").style.color = "blue";
 }
 
+// Nos suscribimos a los clic de cada botón, cada uno a una función diferente, pues
+// hacen cosas distintas
 document.getElementById("rojo").addEventListener("click", ponerRojo);
 document.getElementById("verde").addEventListener("click", ponerVerde);
 document.getElementById("azul").addEventListener("click", ponerAzul);
