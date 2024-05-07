234ae0abadff55c7af1763cc3cc3e16783337b2d
Juan <alonso.jad@gmail.com>
2024-05-07 15:42:16 +0200

    Add files via upload

--- ejemplo16.js
+++ ejemplo16.js
@@ -1,15 +1,18 @@
 function mostrar() {
+    // Obtenemos los valores seleccionados en ambas listas usando la propiedad value
     const local = document.getElementById("local").value;
     const visitante = document.getElementById("visitante").value;
+    // Obtenemos el parrafo partido
     const partido = document.getElementById("partido");
+    // Comprobamos si el equipo local seleccionado es el mismo que el visitante
     if(local == visitante) {
+        // También podemos usar innerText en lugar de textContent, pero no es la forma estándar
         partido.textContent = "Debes elegir equipos diferentes";
     } else {
         partido.textContent = local + " - " + visitante;
-        //document.getElementById("partido").textContent = local + " - " + visitante;
     }
-
 }
 
+// Nos suscribimos al evento click del botón
 document.getElementById("mostrar").addEventListener("click", mostrar);
 
