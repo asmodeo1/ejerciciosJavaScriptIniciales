95c9ee102b62e0c91eae7f37ae571b36c7416efd
Juan <alonso.jad@gmail.com>
2024-05-07 13:53:42 +0200

    Add files via upload

--- ejemplo14.js
+++ ejemplo14.js
@@ -1,10 +1,16 @@
 function visitar() {
     const mayor = document.getElementById("mayor");
+    // Con la propiedad checked comprobamos si la casilla esta marcada
     if(mayor.checked) {
-        window.location.href = "http://www.w3schools.com"
+        /* Con esto vamos a otra página web. No olvidaros de poner http:// o https:// si es
+        una página de otro sitio */
+        window.location.href = "http://www.google.com"
     } else {
+        /* El texto que meustar el mensaje de que debe marcar la casilla está oculto
+        gracias a usar la clase invisible de Bootstrap. Con classList.remove se la quitamos */
         document.getElementById("mensaje").classList.remove("invisible");
     }
 }
 
+// Nos suscribimos al evento click del botón
 document.getElementById("acceder").addEventListener("click", visitar);
