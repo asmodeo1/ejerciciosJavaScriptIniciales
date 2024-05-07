d15fc1929bd984b6ab963047b1d7c5b0fe84509c
Juan <alonso.jad@gmail.com>
2024-05-07 15:55:47 +0200

    Update ejemplo19.js

--- ejemplo19.js
+++ ejemplo19.js
@@ -1,11 +1,20 @@
 function anhadir() {
     const tarea = document.getElementById("tarea");
+    /* Al igual que en el ejercicio 17, nos aseguramos con el método trim que el usuario no haya
+    dejado en blanco o solo con espacios el nombre de la tarea */
     if(tarea.value.trim() != ""){
         const ul = document.getElementById("tareas");
-        const nueva = document.createElement("li")
+        // Creamos un nuevo nodo de tipo li
+        const nuevoLi = document.createElement("li")
+        // Creamos un nuevo nodo de texto con el nombre de la tarea
         const texto = document.createTextNode(tarea.value);
-        nueva.appendChild(texto);
-        ul.appendChild(nueva);
+        // Si no añadimos los nodos creados a algún elemento dentro del documento, se pierden
+        // Añadimos el nodo texto al nuevo li
+        nuevoLi.appendChild(texto);
+        // Añadimos el nodo li a la lista
+        ul.appendChild(nuevoLi);
+        /* Por usubilidad borramos el texto de la tarea y le damos el foco para que el usuario
+        pueda escribir la siguiente tarea */
         tarea.value = "";
         tarea.focus();
     } else {
@@ -14,8 +23,10 @@ function anhadir() {
 }
 
 function vaciar() {
-    document.getElementById("tareas").innerHTML ="";
+    // Eliminamos todo el contenido del ul, es decir, todos sus li
+    document.getElementById("tareas").textContent ="";
 }
 
+// Nos suscribimos a los eventos click de los botones
 document.getElementById("anhadir").addEventListener("click", anhadir);
 document.getElementById("vaciar").addEventListener("click", vaciar);
