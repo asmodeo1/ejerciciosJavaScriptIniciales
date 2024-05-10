aa1991cf45a0ba4d2be3ae9e1b40abea9022763e
Juan <alonso.jad@gmail.com>
2024-05-10 10:55:10 +0200

    Add files via upload

---
+++ arrays/ejemplo_arrays_frases_celebres.js
@@ -0,0 +1,18 @@
+const frasesCelebres = [
+"La vida es lo que pasa mientras estás ocupado haciendo otros planes. - John Lennon",
+"No hay camino para la paz, la paz es el camino. - Mahatma Gandhi",
+"La única forma de hacer un gran trabajo es amar lo que haces. - Steve Jobs",
+"La imaginación es más importante que el conocimiento. - Albert Einstein",
+"Nunca confundas la educación con el aprendizaje. - John Wooden",
+"La mejor venganza es el éxito masivo. - Frank Sinatra",
+"Sé el cambio que quieres ver en el mundo. - Mahatma Gandhi",
+"La felicidad no es algo que pospones para el futuro; es algo que diseñas para el presente. - Jim Rohn",
+"El único límite para nuestra realización de mañana será nuestras dudas de hoy. - Franklin D. Roosevelt",
+"La mejor manera de predecir el futuro es crearlo. - Peter Drucker",
+"El éxito es ir de fracaso en fracaso sin perder el entusiasmo. - Winston Churchill",
+"Si no te gusta algo, cámbialo. Si no puedes cambiarlo, cambia tu actitud. - Maya Angelou",
+"No busques errores, busca soluciones. - Henry Ford",
+"La vida es 10% lo que me pasa y 90% cómo reacciono ante ello. - John Maxwell",
+"Sueña en grande y atrévete a fallar. - Norman Vaughan",
+"No cuentes los días, haz que los días cuenten. - Muhammad Ali",
+];
