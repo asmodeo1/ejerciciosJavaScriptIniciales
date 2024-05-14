21108d99c0dc689845dd1254fb76db0eb74aa231
Juan <alonso.jad@gmail.com>
2024-05-14 16:58:56 +0200

    Add files via upload

--- examen1/ejercicio4.js
+++ examen1/ejercicio4.js
@@ -1,4 +1,7 @@
-/*function calcular() {
+/* Solución del ejercicio sin usar bucle for. Como vemos hay que 
+repetir mucho código
+
+function calcular() {
     const importe1 = document.getElementById("importe1");
     const importe2 = document.getElementById("importe2");
     const importe3 = document.getElementById("importe3");
@@ -11,13 +14,15 @@
     const concepto5 = document.getElementById("concepto5");
     const total = document.getElementById("total");
 
+    // Necesitamos ir acumulando los importes
     let totalImportes = 0;
     if(isNaN(Number(importe1.value)) || isNaN(parseFloat(importe1.value))) {
         importe1.style.backgroundColor = "red";
     } else {
         importe1.style.backgroundColor = "white";
+        // Añadimos el importe al que llevamos acumulado
         totalImportes = totalImportes + parseFloat(importe1.value);
-        // totalImportes += importe1.value;
+        // o totalImportes += importe1.value;
     }
     if(isNaN(Number(importe2.value)) || isNaN(parseFloat(importe2.value))) {
         importe2.style.backgroundColor = "red";
@@ -49,6 +54,7 @@
     }    
     total.value = totalImportes;
 
+    // Vemos si uno está vacío y el otro no
     if(concepto1.value == "" && importe1.value != "") {
         concepto1.style.backgroundColor = "red";
     } else {
@@ -104,21 +110,33 @@
 */
 
 function calcular() {
+    // Cogemos los nodos que tengan un atributo cuyo id comience por concepto
     const conceptos = document.querySelectorAll("[id^='concepto']");
+    // Cogemos los nodos que tengan un atributo cuyo id comience por importe
     const importes = document.querySelectorAll("[id^='importe']");
+    // Necesitamos ir acumulando los importes
     let totalImportes = 0;
 
+    /* Recorremos todos los importes para ir comrpobando si son correctos
+    e ir acumulándolos. Lo más sencillo es usar un for of donde:
+    - importe: es un nombre cualquiera de variable y que irán conteniendo
+    el primer importe, luego el segundo, ...
+    - importes: el array o colección a recorrer*/
     for (const importe of importes) {
         if(isNaN(Number(importe.value)) || isNaN(parseFloat(importe.value))) {
             importe.style.backgroundColor = "red";
         } else {
             importe.style.backgroundColor = "white";
             totalImportes = totalImportes + parseFloat(importe.value);
-            // totalImportes += importe.value;
+            // o totalImportes += importe.value;
         }
     }
     total.value = totalImportes;
 
+    /* Recorremos los conceptos. En este caso no vale un for of, salvo
+    que llevásemos un contador a mano, pues si un concepto está vacío
+    debemos poner su importe correspondiente en rojo. Con for of
+    solo accederíamos a una de las colecciones */
     for (let i = 0; i < conceptos.length; i++) {
         if(conceptos[i].value == "" && importes[i].value != "") {
             conceptos[i].style.backgroundColor = "red";
